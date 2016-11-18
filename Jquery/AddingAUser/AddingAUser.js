e720a48fbc267319817cc4f6ea0f8f99b653deac
TJ <gullo.tj@gmail.com>
2016-11-17 23:00:13 -0500

    AddingAUser and ColorPicker

---
+++ Jquery/AddingAUser/AddingAUser.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+	$("#add-user").click( function(){
+		var first = $("input[name='first-name']").val();
+		var last = $("input[name='last-name']").val();
+		var email = $("input[name='email']").val();
+		var phone = $("input[name='contact-num'").val();
+		$("tbody").append("<tr>" + "<td>" + first + "</td>" + "<td>" + last + "</td>" + "<td>" + email + "</td>" + "<td>" + phone + "</td>" + "</tr>");
+	});
+});
