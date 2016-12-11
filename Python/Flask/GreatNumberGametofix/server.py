02c3c2726120c5b1947900a394be3281a4a94b1b
TJGullo <gullo.tj@gmail.com>
2016-12-10 23:22:10 -0500

    Fixed great number game to fix =)

--- Python/Flask/GreatNumberGametofix/server.py
+++ Python/Flask/GreatNumberGametofix/server.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect, session
 app = Flask(__name__)
 app.secret_key = 'Super duper secret'
 import random
-from random import randint
+
 
 @app.route('/')
 def index():
@@ -14,8 +14,13 @@ def index():
 
 @app.route('/guess', methods=['POST'])
 def guess():
-    session['guess'] = int(request.form['number'])
-    guess = session['guess']
+    print '\n', '***'*50, 'we made it to the guessing route, guess = ', request.form['number']
+    try:
+        session['guess'] = int(request.form['number'])
+    except ValueError:
+        session['guess'] = 0
+    #guess = session['guess']
+
 
     return render_template('index.html')
 
