97e1bde46299516ffb59453bf16cf812bc4a0e52
TJGullo <gullo.tj@gmail.com>
2017-01-21 15:29:46 -0500

    Updated emailvalidation

--- Python/Django/email_validation/apps/validation/models.py
+++ Python/Django/email_validation/apps/validation/models.py
@@ -12,18 +12,18 @@ class EmailManager(models.Manager):
 
         if len(email) < 1:
             errors.append('email field must be filled out')
-        elif not re.match(email, EMAIL_REGEX):
+        elif not re.match(EMAIL_REGEX, email):
             errors.append('email must be valid')
 
         if errors:
             return [False, errors]
         else:
-            email = Email.emailManager.create(email=email)
+            email = self.create(email=email)
             print email
             return [True, email]
 
 class Email(models.Model):
     email = models.EmailField()
-    emailManager = EmailManager()
+    objects = EmailManager()
     created_at = models.DateTimeField(auto_now_add = True)
     updated_at = models.DateTimeField(auto_now = True)
--- Python/Django/email_validation/apps/validation/views.py
+++ Python/Django/email_validation/apps/validation/views.py
@@ -9,22 +9,24 @@ def validation(request):
     print 'in process method'
     print request.POST
 
-    response = Email.emailManager.isValidEmail(request.POST)
+    response = Email.objects.isValidEmail(request.POST)
 
     if response[0] == False:
         for error in response[1]:
             messages.error(request, error)
             return redirect('/')
     else:
-        return redirect('validation/success.html')
+        print response[1]
+        request.session['curr_email'] = response[1].email
+        return redirect('/success')
 
 
 
 
 
 def success(request):
-    emails = Email.emailManage.all()
+    emails = Email.objects.all()
     context = {
-    'curr_email': request.session('email')
+    'emails': emails
     }
     return render(request, 'validation/success.html', context)
